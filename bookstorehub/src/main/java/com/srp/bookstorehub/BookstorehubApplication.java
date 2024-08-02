60cbfbefbe0d59685180a69b7a591298d5c7a37a
lenovo <lenovo@LAPTOP-434RVCBE>
2024-08-03 00:26:48 +0530

    base project setup

---
+++ bookstorehub/src/main/java/com/srp/bookstorehub/BookstorehubApplication.java
@@ -0,0 +1,15 @@
+package com.srp.bookstorehub;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstorehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstorehubApplication.class, args);
+	}
+
+}
+
